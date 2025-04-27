51e7391cd6cc4387e2a8750354b6fbbc1966a6e1
kedoshim <abraaodpaula@gmail.com>
2025-04-27 15:59:45 -0300

    Versão 1

---
+++ README.md
@@ -0,0 +1,2 @@
+# Dia-das-Maes-AmoVoce
+A menu like site for the breakfast basket manufactures "AmoVocê Cestas"
---
+++ js/products.js
@@ -0,0 +1,25 @@
+export const products = [
+    {
+        title: "Abraço de mãe",
+        price: "189,90",
+        description: "",
+        items: "Bolsa reutilizável • Chaveiro decorativo com letra do nome da mãe • Xicara de porcelana • Suco de frutas 290ml • Café-Drip Coffee • Croissant recheado • Brownie recheado • Pote de Geleia • Torradas • Biscoitos artesanais de canela • Frios: Peito peru/mussarela/queijo prato • Pão com cobertura de queijo parmesão • Mini arranjo de flores • Cartão especial Dia das Mães",
+        images: {
+            1: "img/bolsa/1.png",
+            2: "img/bolsa/2.png",
+            3: "img/bolsa/3.png",
+            4: "img/bolsa/4.png",
+        },
+    },
+    {
+        title: "Afeto Presente",
+        price: "139,90",
+        description: "",
+        items: "Cesto de palha reutilizável • Suco de frutas 290ml • Geleia de frutas • Biscoitos artesanais de canela • Torradas • Croissants recheados • Brownie recheado • Mini buquê de flores secas • Cartão especial Dia das Mães",
+        images: {
+            1: "img/caixa/5.png",
+            2: "img/caixa/6.png",
+            3: "img/caixa/7.png",
+        },
+    },
+];
