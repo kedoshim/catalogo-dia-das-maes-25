0fb134c5cab222118223374f2b7aad287690e9ac
kedoshim <abraaodpaula@gmail.com>
2025-04-28 17:09:55 -0300

    Chaveiro->Adereco

--- js/products.js
+++ js/products.js
@@ -3,7 +3,7 @@ export const products = [
         title: "Abraço de mãe",
         price: "189,90",
         description: "",
-        items: "Bolsa reutilizável • Chaveiro decorativo com letra do nome da mãe • Xicara de porcelana • Suco de frutas 290ml • Café-Drip Coffee • Croissant recheado • Brownie recheado • Pote de Geleia • Torradas • Biscoitos artesanais de canela • Frios: Peito peru/mussarela/queijo prato • Pão com cobertura de queijo parmesão • Mini arranjo de flores • Cartão especial Dia das Mães",
+        items: "Bolsa reutilizável • Adereço decorativo com letra do nome da mãe • Xicara de porcelana • Suco de frutas 290ml • Café-Drip Coffee • Croissant recheado • Brownie recheado • Pote de Geleia • Torradas • Biscoitos artesanais de canela • Frios: Peito peru/mussarela/queijo prato • Pão com cobertura de queijo parmesão • Mini arranjo de flores • Cartão especial Dia das Mães",
         images: {
             1: "img/bolsa/1.png",
             2: "img/bolsa/2.png",
